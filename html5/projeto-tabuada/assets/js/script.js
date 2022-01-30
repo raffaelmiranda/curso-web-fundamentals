e9e8a21bcb2768e3ffb46dbce1715619cecb05e7
Rafael Miranda <rafael.miranda@solutis.com.br>
2022-01-30 14:07:51 -0300

    projeto tabuada

---
+++ html5/projeto-tabuada/assets/js/script.js
@@ -0,0 +1,24 @@
+function calcularTabuada() {
+  let tabuada = document.querySelector("#tabuada tbody");
+
+  let valorA = parseInt(document.querySelector("#valorA").value);
+  tabuada.innerHTML = "";
+  for (let valorB = 0; valorB <= 10; valorB++) {
+    let resultado = valorA * valorB;
+
+    let template = `
+      <td>${valorA}</td>
+      <td>X</td>
+      <td>${valorB}</td>
+      <td>=</td>
+      <td>${resultado}</td>
+    `;
+
+    let tr = document.createElement("tr");
+    tr.innerHTML = template;
+
+    tabuada.append(tr);
+  }
+}
+
+document.querySelector("#valorA").addEventListener("change", calcularTabuada);
